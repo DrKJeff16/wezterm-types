0022b09e6ff040db9d3871f54073f75ce3498b38
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-09-12 16:23:59 -0600

    fix(wezterm): completed `wezterm.url` docs
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/wezterm/types/wezterm/url.lua
+++ lua/wezterm/types/wezterm/url.lua
@@ -1,27 +1,66 @@
 ---@meta
 
----@class UrlObject
----The URL scheme such as "file", or "https"
+---The `Url` object represents a parsed URL.
+---
+---An example:
+---
+---```lua
+---local wezterm = require 'wezterm'
+---
+---local url = wezterm.url.parse 'file://myhost/some/path%20with%20spaces'
+---assert(url.scheme == 'file')
+---assert(url.file_path == '/some/path with spaces')
+---
+---local url =
+---  wezterm.url.parse 'https://github.com/rust-lang/rust/issues?labels=E-easy&state=open'
+---assert(url.scheme == 'https')
+---assert(url.username == '')
+---assert(url.password == nil)
+---assert(url.host == 'github.com')
+---assert(url.path == '/rust-lang/rust/issues')
+---assert(url.query == 'labels=E-easy&state=open')
+---```
+---
+---@class Url
+---The URL scheme such as `"file"`, or `"https"`.
+---
 ---@field scheme string|"file"|"https"|"http"
----Decodes the path field and interprets it as a file path
+---Decodes the path field and interprets it as a file path.
+---
 ---@field file_path string
----The `username` portion of the URL, or an empty string if none is specified
+---The `username` portion of the URL, or an empty string
+---if none is specified.
+---
 ---@field username string|""
----The password portion of the URL, or `nil` if none is specified
+---The password portion of the URL, or `nil` if none is specified.
+---
 ---@field password string|nil
----The `hostname` portion of the URL, with IDNA decoded to UTF-8
+---The `hostname` portion of the URL, with IDNA decoded to UTF-8.
+---
 ---@field host string
----The `path` portion of the URL, complete with percent encoding
+---The `path` portion of the URL, complete with percent encoding.
+---
 ---@field path string
----The `fragment` portion of the URL
+---The `fragment` portion of the URL.
+---
 ---@field fragment string
----The `query` portion of the URL
+---The `query` portion of the URL.
+---
 ---@field query string
 
----The `wezterm.url` module exposes functions that allow working with URLs
+---The `wezterm.url` module exposes functions that
+---allow working with URLs.
+---
 ---@class Wezterm.Url
----The `Url` object represents a parsed Url. It has the following fields:
----@field Url UrlObject
+---The `Url` object represents a parsed URL.
+---
+---@field Url Url
+local M = {}
+
 ---Attempts to parse the provided URL_STRING as a URL.
----If success, returns a `UrlObject` type representing that URL
----@field parse fun(url_string: string): UrlObject
+---
+---If successful, returns a `Url` object representing said URL.
+---
+---@param url_string string
+---@return Url?
+function M.parse(url_string) end
