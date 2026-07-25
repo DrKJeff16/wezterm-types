f4bd08dfbab1c3de6ffb55dc9166c0c7e9726c6b
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-07-24 22:56:22 -0600

    fix(plugins.quota-limit): update type annotations
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/wezterm/types/plugins/quota-limit.lua
+++ lua/wezterm/types/plugins/quota-limit.lua
@@ -1,16 +1,26 @@
 ---@meta
 ---@diagnostic disable:unused-local
 
----@alias QuotaLimitOpts.Position "left"|"right"
-
 ---@class QuotaLimitOpts.Icons
 ---@field bolt? string
 ---@field week? string
 
+---@class QuotaLimitOpts.DashboardKey: SendKeyParams
+---@field mods string
+
+---@class QuotaLimitOpts.Colors
+---@field yellow? string
+---@field red? string
+---@field green? string
+---@field dim? string
+---@field bright? string
+
 ---@class QuotaLimitOpts
----@field poll_interval_secs? integer
----@field position? QuotaLimitOpts.Position
+---@field colors? "auto"|"dark"|"light"|QuotaLimitOpts.Colors
+---@field dashboard_key? QuotaLimitOpts.DashboardKey
 ---@field icons? QuotaLimitOpts.Icons
+---@field poll_interval_secs? integer
+---@field position? "left"|"right"
 
 ---@class QuotaLimit
 local M = {}
